da952022df1d44e78eabdbfdea4eb871f2b006b4
Jiani Li <lijiani@lijianideMacBook-Pro.local>
2022-05-02 16:48:14 -0400

    upload question 198.

---
+++ q176-200/q198.java
@@ -0,0 +1,17 @@
+// 2022.05.02
+// Problem Statement:
+// https://leetcode.com/problems/house-robber/
+
+// idea: dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length==1) return nums[0];
+        int [] dp = new int [nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i=2; i<nums.length; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        return dp[nums.length-1];
+    }
+}
