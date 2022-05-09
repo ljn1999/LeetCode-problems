c59b10b4d1ed14a13880acaa9244b16a64836be3
Jiani Li <lijiani@lijianideMacBook-Pro.local>
2022-05-09 14:12:32 -0400

    upload question 217.

---
+++ q201-225/q217.java
@@ -0,0 +1,16 @@
+// 2022.05.09
+// Problem Statement:
+// https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Boolean> exist = new HashMap<Integer, Boolean> ();
+        for (int i=0; i<nums.length; i++) {
+            if (exist.containsKey(nums[i])) return true;
+            else {
+                exist.put(nums[i], true);
+            }
+        }
+        return false;
+    }
+}
